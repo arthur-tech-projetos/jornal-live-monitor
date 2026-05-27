24ed2231d29b18de409f3677a037f3861d0a5bdf
arthur-tech-projetos <arthurxxz220@gmail.com>
2026-05-27 12:53:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const CHANNEL_ID = 'UCEXZddw6rp2Nu76ibj9e8SQ';
 const TELEGRAM_TOKEN = '8881818050:AAFZSOn231TQXWiuvyfJX_xq7LIjrbhStlA';
 
 // MUDANÇA AQUI: Usando 'let' para o bot aprender o ID sozinho!
-let TELEGRAM_CHAT_ID = '-5294989968'; 
+let TELEGRAM_CHAT_ID = '-1003937290720'; 
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://arthur:Arthur12%40XP@cluster0.nrt11po.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
